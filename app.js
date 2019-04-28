c525849ec9378d41e0e69efa795cb980e84bd36f
PoppyCurtis <poppy.curtis@gmail.com>
2019-04-28 11:42:21 +0100

    Use a .env file to store secret information

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -17,7 +18,7 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-const secret = "Thisismysecret";
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
